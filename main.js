66b7fa509ae0f25144afc0cd2e8a2bd67a618171
Le Trong Dat <datlt2306@gmail.com>
2023-11-01 10:28:46 +0700

    init project

---
+++ main.js
@@ -0,0 +1,26 @@
+const myName = "Le Trong Dat"; // string
+const myAge = 20; // number
+const isStatus = true; // boolean
+const listName = ["Dat", "Chinh", "Dung"]; // object
+//index             0      1        2
+
+console.log("Toi ten la " + myName + ", Nam nay toi " + myAge + " Tuoi"); // Le Trong Dat
+console.log(`Toi ten la ${myName}, Nam nay toi ${myAge} Tuoi`); // Le Trong Dat
+console.log(listName[2]); // true
+
+// Cách đặt tên biến
+// 1. Không bắt đầu bằng số
+// 2. Không chứa kí tự đặc biệt
+// 3. Không trùng với từ khóa của ngôn ngữ
+
+// Kiểu dữ liệu
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Null
+
+/**
+ * 1. git init
+ * 2. git remote add origin [link repository]
+ * 3. git add .
+ */
